e52bb0695ebf52f2901e217033c56e117d9dfe93
Khrystyna <>
2021-01-06 16:59:13 -0600

    first commit

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/<username>/<int:post_id>')
-def hello_world(username=None, post_id=None):
-    return render_template('index.html', name=username, post_id=post_id)
+@app.route('/')
+def my_home():
+    return render_template('index.html')
 
 
 @app.route('/about.html')
