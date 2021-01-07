e2ac2d97c23909a3ea7d48c62408673c22643352
Khrystyna <>
2021-01-06 20:30:21 -0600

    first commit

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 print(__name__)
 
@@ -7,11 +7,16 @@ def my_home():
     return render_template('index.html')
 
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'Hello, dogs!'
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong, try again'
