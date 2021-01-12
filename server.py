913c283931e1d400ddb21c76a6334888e6b9efb8
Khrystyna <>
2021-01-12 14:08:14 -0600

    first commit

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
+import csv
+
 app = Flask(__name__)
-print(__name__)
+
 
 @app.route('/')
 def my_home():
@@ -12,11 +15,26 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as database:
+        name = data['name']
+        email = data['email']
+        message = data['message']
+        date_created = datetime.now().replace(second=0, microsecond=0)
+        csv_writer = csv.writer(database, delimiter=',',  
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL
+                                )
+        csv_writer.writerow([name, email, message, date_created])
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        print(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
     else:
         return 'something went wrong, try again'
