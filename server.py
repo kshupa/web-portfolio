98636d32825bcd0a82919854a9eb00cb181e2aeb
Khrystyna <>
2021-02-04 20:37:09 -0600

    heroku setup

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ app.config.update(
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT=465,
     MAIL_USE_SSL=True,
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME'),
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD'),
+    MAIL_USERNAME = os.environ.get['MAIL_USERNAME'],
+    MAIL_PASSWORD = os.environ.get['MAIL_PASSWORD'],
 )
 
 mail = Mail(app)
