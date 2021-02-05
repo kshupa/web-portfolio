98582c0791f430a949d0a305a0d3fba86319711f
Khrystyna <>
2021-02-04 21:14:15 -0600

    some cleanup

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect
 from flask_mail import Message, Mail
-# from config import mail_username, mail_password
 import os
 
 app = Flask(__name__)
